149f78a6a2c6ec6d2db0af35823e9aa339a1b46d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-08-30 14:25:32 +0200

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -1,37 +1,22 @@
-from flask import Flask, render_template
-from graph_visualizer import GraphVisualizer
-from database_manager import DatabaseManager  # Adjust the import according to your structure
+from flask import Flask, render_template, jsonify
+from database_manager import DatabaseManager
 
 class FlaskApp:
     def __init__(self, db_manager):
         self.app = Flask(__name__)
         self.db_manager = db_manager
-        self.graph_visualizer = GraphVisualizer()
 
         @self.app.route('/')
         def index():
+            # Query unique values from column A
             query = 'SELECT DISTINCT A FROM table1 UNION SELECT DISTINCT A FROM table2'
             unique_values = self.db_manager.query_database(query)
             unique_values = [item[0] for item in unique_values]
             return render_template('index.html', unique_values=unique_values)
 
-        @self.app.route('/graph/<value>', methods=['GET'])
-        def graph(value):
-            query1 = 'SELECT D FROM table1 WHERE A = ?'
-            query2 = 'SELECT D FROM table2 WHERE A = ?'
-            data1 = self.db_manager.query_database(query1, (value,))
-            data2 = self.db_manager.query_database(query2, (value,))
-
-            combined_data = {
-                'parent': value,
-                'children': {
-                    'table1': [d[0] for d in data1],
-                    'table2': [d[0] for d in data2]
-                }
-            }
-
-            # Create and generate Plotly graph
-            G = self.graph_visualizer.create_graph(value, combined_data['children'])
-            graph_html = self.graph_visualizer.generate_plotly_graph(G)
-
-            return render_template('graph.html', graph_html=graph_html)
+        @self.app.route('/data')
+        def data():
+            # Return data in JSON format for hexbin map
+            query = 'SELECT A FROM table1 UNION SELECT A FROM table2'
+            data = self.db_manager.query_database(query)
+            return jsonify([item[0] for item in data])
