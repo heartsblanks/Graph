c411030997f6223bee9ed97f37bbe3f4c1107a9d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-08-30 17:15:34 +0200

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -34,30 +34,26 @@ class GraphVisualizer:
         import plotly.io as pio
         return pio.to_html(graph, full_html=False)
 
-@app.route('/graph/<value>', methods=['GET'])
-def graph(value):
-    db_manager = DatabaseManager()
-    graph_visualizer = GraphVisualizer()
-    
-    query1 = 'SELECT D FROM table1 WHERE A = ?'
-    query2 = 'SELECT D FROM table2 WHERE A = ?'
-    
-    data1 = db_manager.query_database(query1, (value,))
-    data2 = db_manager.query_database(query2, (value,))
-    
-    combined_data = {
-        'parent': value,
-        'children': {
-            'table1': [d[0] for d in data1],
-            'table2': [d[0] for d in data2]
-        }
-    }
-    
-    # Create and generate Plotly graph
-    G = graph_visualizer.create_graph(value, combined_data['children'])
-    graph_html = graph_visualizer.generate_plotly_graph(G)
-    
-    return jsonify({'graph_html': graph_html, 'data': combined_data})
+@self.app.route('/graph/<value>', methods=['GET'])
+        def graph(value):
+            query1 = 'SELECT D FROM table1 WHERE A = ?'
+            query2 = 'SELECT D FROM table2 WHERE A = ?'
+            data1 = self.db_manager.query_database(query1, (value,))
+            data2 = self.db_manager.query_database(query2, (value,))
+
+            combined_data = {
+                'parent': value,
+                'children': {
+                    'table1': [d[0] for d in data1],
+                    'table2': [d[0] for d in data2]
+                }
+            }
+
+            # Create and generate Plotly graph
+            G = self.graph_visualizer.create_graph(value, combined_data['children'])
+            graph_html = self.graph_visualizer.generate_plotly_graph(G)
+
+            return render_template('graph.html', graph_html=graph_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
