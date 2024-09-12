1d5f26a8daf3fb2327c566c8e414fca068ecb1d1
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2024-09-12 11:21:25 +0200

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -26,41 +26,28 @@ class FlaskApp:
             data2 = self.db_manager.query_database(query2, (value,))
 
             # Combine data from both tables
-            destinations = data1 + data2  # This will contain D, F, and G now
-
-            # Function to extract substring between the 2nd and 3rd underscores
-            def extract_between_underscores(destination):
-                if isinstance(destination, str):
-                    parts = destination.split('_')
-                    if len(parts) >= 4:
-                        return parts[2]  # The part between the 2nd and 3rd underscores
-                return None
-
-            # Process destinations to extract the part between 2nd and 3rd underscores
-            processed_destinations = [
-                (extract_between_underscores(dest[0]), dest[1], dest[2])  # dest[0] is D, dest[1] is F, dest[2] is G
-                for dest in destinations if extract_between_underscores(dest[0])
-            ]
-            processed_destinations = list(set(processed_destinations))  # Remove duplicates
-
-            # Prepare graph data
-            nodes = [{'id': value, 'group': 1}]  # The source node
             links = []
+            destinations = []
 
-            # Add destination nodes and links with F-G data
-            for dest, column_f, column_g in processed_destinations:
-                if dest not in [node['id'] for node in nodes]:
-                    nodes.append({'id': dest, 'group': 2})  # Destination nodes
+            for d in data1 + data2:
+                dest, f_data, g_data = d
+                if dest not in destinations:
+                    destinations.append(dest)
+
+                # Add to links with f_data and g_data for display on the link
                 links.append({
-                    'source': value, 
-                    'target': dest, 
-                    'f_data': column_f,  # F data
-                    'g_data': column_g   # G data
+                    'source': value,
+                    'target': dest,
+                    'f_data': f_data,
+                    'g_data': g_data
                 })
 
-            graph_data = {'nodes': nodes, 'links': links}
-            
-            return jsonify(graph_data)  # Return the data as JSON
+            # Remove duplicates and prepare graph data
+            destinations = list(set(destinations))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+            nodes = [{'id': value, 'group': 1}]  # The source node
+            for dest in destinations:
+                nodes.append({'id': dest, 'group': 2})  # Destination nodes
+
+            graph_data = {'nodes': nodes, 'links': links}
+            return jsonify(graph_data)
